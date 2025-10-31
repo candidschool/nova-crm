359c29da76fec4bd5128bff50e8c6c8b62efb55d
candidschool <comms@candidschools.com>
2025-11-01 00:59:37 +0530

    Update Stage1ActionButton.jsx

--- src/components/Stage1ActionButton.jsx
+++ src/components/Stage1ActionButton.jsx
@@ -110,8 +110,8 @@ const Stage1ActionButton = ({
         userName: leadData.parentsName,
         templateParams: [leadData.parentsName],
         media: {
-          url: 'https://candidschools.com/NOVA_B.pdf',
-          filename: 'NOVA_B.pdf'
+          url: 'https://candidschools.com/NOVA_Brochure.pdf',
+          filename: 'NOVA_Brochure.pdf'
         }
       };
 
