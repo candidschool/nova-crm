6358816fff1b38aef15edac5c3c486ee3719275b
candidschool <comms@candidschools.com>
2025-12-23 18:18:37 +0530

    Update LeadsTable.jsx

--- src/components/LeadsTable.jsx
+++ src/components/LeadsTable.jsx
@@ -1119,7 +1119,7 @@ if (oldStageKey !== newStageKey) {
         <div className="nova-table-container" style={{ marginTop: (counsellorFilters.length > 0 || 
                 stageFilters.length > 0 || 
                 statusFilters.length > 0 || 
-                sourceFilters.length > 0) ? '45px' : '30px'}}>
+                sourceFilters.length > 0) ? '65px' : '30px'}}>
           <table className="nova-table">
             <thead>
               <tr>
